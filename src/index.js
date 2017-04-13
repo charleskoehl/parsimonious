0c4897e836b47e9b8a6ee56a4d52c92b721abe2b
Charles Koehl <ckoehl@leisurelink.com>
2017-04-12 14:42:41 -1000

    First commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { merge, pick } from 'lodash';
+
+module.exports = {
+  /**
+   * Get some columns from a Parse object and returns them in a js object
+   * @param {Parse.Object} parseObj
+   * @param {(string|string[])} keys
+   * @returns {object}
+   */
+  objPick: (parseObj, keys) => {
+    const keysArr = Array.isArray(keys) ? keys : keys.split(',');
+    return pick(parseObj.toJSON(), keysArr);
+  },
+  /**
+   * Sets some columns on a Parse object from a js object.
+   * Mutates parseObj
+   * @param {Parse.Object} parseObj
+   * @param {array|string} keys
+   */
+  objSetMulti: (parseObj, dataObj, doMerge = false) => {
+    let key, newVal;
+    for (key in dataObj) {
+      newVal = dataObj[key];
+      if (doMerge) {
+        newVal = merge(parseObj.get(key), newVal);
+      }
+      parseObj.set(key, newVal);
+    }
+  }
+};
---
+++ src/index.spec.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import Parse from 'Parse';
+import utils from './index';
+
+describe('utils.objPick', ()=>{
+  it('gets some columns from a Parse object and returns them in a js object', ()=>{
+    const parseObj = new Parse.Object('Colors');
+    parseObj.set('roses', 'red');
+    parseObj.set('violets', 'blue');
+    parseObj.set('grass', 'green');
+    expect(utils.objPick(parseObj, 'roses,grass')).to.eql({roses:'red', grass:'green'});
+    expect(utils.objPick(parseObj, ['roses', 'grass'])).to.eql({roses:'red', grass:'green'});
+  });
+  it('sets some columns on a Parse object from a js object', ()=>{
+    const parseObj = new Parse.Object('Colors');
+    parseObj.set('roses', 'red');
+    parseObj.set('violets', 'blue');
+    parseObj.set('grass', 'green');
+    utils.objSetMulti(parseObj, {valley:'big', river:'deep'});
+    expect(parseObj.get('river')).to.equal('deep');
+  });
+});
