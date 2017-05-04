57dc346d2cc0d10160fb9d85b0d7f129e2a044e1
Charles Koehl <ckoehl@leisurelink.com>
2017-05-04 00:40:35 -1000

    added tests for toJsn recursive and getClassInst

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,38 +5,52 @@ import parsimonious from '../src/index'
 const Colors = Parse.Object.extend('Colors')
 const colors = new Colors()
 
-describe('parsimonious.toJsn()', () => {
-  it('returns JSON representation of a Parse object', () => {
+describe('parsimonious methods', () => {
+  
+  describe('toJsn()', () => {
     colors.set('roses', 'red')
     colors.set('violets', 'blue')
     colors.set('grass', 'green')
-    expect(parsimonious.toJsn(colors)).to.eql({roses: 'red', violets: 'blue', grass: 'green'})
+    it('returns a shallow JSON representation of a Parse object', () => {
+      expect(parsimonious.toJsn(colors)).to.eql({roses: 'red', violets: 'blue', grass: 'green'})
+    })
+    it('returns deep JSON representation of a plain object containing a Parse object', () => {
+      const plainObj = {foo:'bar',domo:'arigato',things:['cow','pencil'], colors}
+      expect(parsimonious.toJsn(plainObj, true)).to.eql({foo:'bar',domo:'arigato',things:['cow','pencil'], colors: {roses: 'red', violets: 'blue', grass: 'green'}})
+    })
   })
-})
-
-describe('parsimonious.objPick()', () => {
-  it('gets some columns from a Parse object and returns them in a js object', () => {
-    expect(parsimonious.objPick(colors, 'roses,grass')).to.eql({roses: 'red', grass: 'green'})
-    expect(parsimonious.objPick(colors, ['roses', 'grass'])).to.eql({roses: 'red', grass: 'green'})
+  
+  describe('objPick()', () => {
+    it('gets some columns from a Parse object and returns them in a plain object', () => {
+      expect(parsimonious.objPick(colors, 'roses,grass')).to.eql({roses: 'red', grass: 'green'})
+      expect(parsimonious.objPick(colors, ['roses', 'grass'])).to.eql({roses: 'red', grass: 'green'})
+    })
   })
-})
-
-describe('parsimonious.objSetMulti()', () => {
-  it('sets some columns on a Parse object from a js object', () => {
-    parsimonious.objSetMulti(colors, {valley: 'big', river: 'deep'})
-    expect(colors.get('river')).to.equal('deep')
+  
+  describe('objSetMulti()', () => {
+    it('sets some columns on a Parse object from a js object', () => {
+      parsimonious.objSetMulti(colors, {valley: 'big', river: 'deep'})
+      expect(colors.get('river')).to.equal('deep')
+    })
   })
-})
-
-describe('parsimonious.newQuery()', () => {
-  const query = parsimonious.newQuery('Colors')
-  it('generates a new Parse.Query given a Parse Object class name', () => {
-    expect(typeof query === 'object' && query.className === 'Colors' && typeof query.find === 'function').to.be.true
+  
+  describe('newQuery()', () => {
+    const query = parsimonious.newQuery('Colors')
+    it('returns a new Parse.Query given a Parse Object class name', () => {
+      expect(typeof query === 'object' && query.className === 'Colors' && typeof query.find === 'function').to.be.true
+    })
   })
-})
-
-describe('parsimonious.isPFObject()', () => {
-  it('determines if a variable is a Parse.Object', () => {
-    expect(parsimonious.isPFObject(colors)).to.be.true
+  
+  describe('getClassInst()', () => {
+    const inst = parsimonious.getClassInst('Colors')
+    it('returns a subclass of Parse.Object given class name', () => {
+      expect(inst.className === 'Colors').to.be.true
+    })
+  })
+  
+  describe('isPFObject()', () => {
+    it('determines if a variable is a Parse.Object', () => {
+      expect(parsimonious.isPFObject(colors)).to.be.true
+    })
   })
 })
