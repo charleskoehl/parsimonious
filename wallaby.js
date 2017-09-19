3ac4ea7649c59fb3e492a242f817ddf97ae24818
Charles Koehl <charles@etherscapes.com>
2017-09-18 19:01:05 -1000

    configure wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,18 +1,20 @@
 module.exports = function (wallaby) {
-    return {
-        files: [
-            {pattern: 'node_modules/chai/chai.js', instrument: false},
-            'src/**/*.js'
-        ],
-        tests: [
-            'test/**/*.js'
-        ],
-        testFramework: 'mocha',
-        compilers: {
-            '**/*.js': wallaby.compilers.babel()
-        },
-        env: {
-            type: 'node'
-        }
-    };
-};
+  return {
+    files: [
+      {pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false},
+      {pattern: 'src/**/*.*.test.js', ignore: true},
+      'src/**/*.js'
+    ],
+    tests: [
+      'src/**/*.*.test.js'
+    ],
+    testFramework: 'jest',
+    compilers: {
+      '**/*.js': wallaby.compilers.babel()
+    },
+    env: {
+      type: 'node',
+      runner: 'node'
+    }
+  }
+}
