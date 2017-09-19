d7d056a1db1c0a562b2c633c7b763f6542f9ca12
Charles Koehl <charles@etherscapes.com>
2017-09-18 19:05:51 -1000

    fully cover toJsn with tests

--- src/index.unit.test.js
+++ src/index.unit.test.js
@@ -4,9 +4,37 @@ import parsm from './index'
 const Colors = Parse.Object.extend('Colors')
 const colors = new Colors()
 
+expect.extend({
+  toBeEquivalentToObject(received, argument) {
+    let pass = true
+    const rProps = Object.getOwnPropertyNames(received)
+    const aProps = Object.getOwnPropertyNames(argument)
+    if (rProps.length != aProps.length) {
+      pass = false
+    }
+    for (let i = 0; i < rProps.length; i++) {
+      var propName = rProps[i]
+      if (received[propName] !== argument[propName]) {
+        pass = false
+      }
+    }
+    if(pass) {
+      return {
+        message: () => (`expected ${received} to not contain the same properties and values as ${argument}`),
+        pass: true
+      }
+    } else {
+      return {
+        message: () => (`expected ${received} to contain the same properties and values as ${argument}`),
+        pass: false
+      }
+    }
+  }
+})
+
 describe('parsimonious methods', () => {
   
-  describe('toJsn()', () => {
+  describe('toJsn', () => {
     colors.set('roses', 'red')
     colors.set('violets', 'blue')
     colors.set('grass', 'green')
@@ -17,27 +45,68 @@ describe('parsimonious methods', () => {
         grass: 'green'
       })
     })
-    it('returns deep JSON representation of a plain object containing a Parse object', () => {
+    it('returns deep JSON representation of a plain object containing a plain, non-Parse object', () => {
       const plainObj = {
-        foo: 'bar', 
-        domo: 'arigato', 
-        things: ['cow', 'pencil'], 
-        colors
+        foo: 'bar',
+        domo: 'arigato',
+        things: ['cow', 'pencil'],
+        doohicky: {
+          springs: 5,
+          levers: 2
+        }
       }
       expect(parsm.toJsn(plainObj, true)).toEqual({
         foo: 'bar',
         domo: 'arigato',
         things: ['cow', 'pencil'],
+        doohicky: {
+          springs: 5,
+          levers: 2
+        }
+      })
+    })
+    it('returns deep JSON representation of a plain object containing a Parse object', () => {
+      const someObj = {
+        foo: 'bar',
+        domo: 'arigato',
+        things: ['cow', 'pencil'],
+        colors
+      }
+      expect(parsm.toJsn(someObj, true)).toEqual({
+        foo: 'bar',
+        domo: 'arigato',
+        things: ['cow', 'pencil'],
         colors: {
-          roses: 'red', 
-          violets: 'blue', 
+          roses: 'red',
+          violets: 'blue',
           grass: 'green'
         }
       })
     })
+    it('returns deep JSON representation of a plain object containing a Parse object that has an objectId key', () => {
+      const myColors = new Colors()
+      myColors.set('roses', 'red')
+      myColors.set('violets', 'blue')
+      myColors.set('grass', 'green')
+      myColors.set('objectId', 'iei38s')
+      const someObj = {
+        foo: 'bar',
+        domo: 'arigato',
+        things: ['cow', 'pencil'],
+        colors: myColors
+      }
+      const json = parsm.toJsn(someObj, true)
+      expect(json.foo).toEqual('bar')
+      expect(json.domo).toEqual('arigato')
+      expect(json.things).toEqual(['cow', 'pencil'])
+      expect(json.colors.grass).toEqual('green')
+      expect(json.colors.roses).toEqual('red')
+      expect(json.colors.violets).toEqual('blue')
+      expect(json.colors.id).toEqual('iei38s')
+    })
   })
   
-  describe('objPick()', () => {
+  describe('objPick', () => {
     it('gets some columns from a Parse object and returns them in a plain object', () => {
       expect(parsm.objPick(colors, 'roses,grass')).toEqual({
         roses: 'red',
@@ -50,7 +119,7 @@ describe('parsimonious methods', () => {
     })
   })
   
-  describe('objSetMulti()', () => {
+  describe('objSetMulti', () => {
     it('sets some columns on a Parse object from a js object', () => {
       parsm.objSetMulti(colors, {
         valley: 'big',
@@ -60,21 +129,45 @@ describe('parsimonious methods', () => {
     })
   })
   
-  describe('newQuery()', () => {
+  describe('getMkStOpts', () => {
+    it('returns null when no params', () => {
+      expect(parsm.getMkStOpts()).toEqual(null)
+    })
+    it('returns null when params are falsy', () => {
+      expect(parsm.getMkStOpts(false)).toEqual(null)
+      expect(parsm.getMkStOpts(null)).toEqual(null)
+      expect(parsm.getMkStOpts(false,null)).toEqual(null)
+      expect(parsm.getMkStOpts(null,null)).toEqual(null)
+    })
+  })
+  
+  describe('newQuery', () => {
     const query = parsm.newQuery('Colors')
     it('returns a new Parse.Query given a Parse Object class name', () => {
       expect(typeof query === 'object' && query.className === 'Colors' && typeof query.find === 'function').toBe(true)
     })
   })
   
-  describe('getClassInst()', () => {
+  describe('getObjById', () => {
+  
+  })
+  
+  describe('getUserById', () => {
+  
+  })
+  
+  describe('userHasRole', () => {
+  
+  })
+  
+  describe('getClassInst', () => {
     const inst = parsm.getClassInst('Colors')
     it('returns a subclass of Parse.Object given class name', () => {
       expect(inst.className === 'Colors').toBe(true)
     })
   })
   
-  describe('isPFObject()', () => {
+  describe('isPFObject', () => {
     it('determines if a variable is a Parse.Object', () => {
       expect(parsm.isPFObject(colors)).toBe(true)
     })
